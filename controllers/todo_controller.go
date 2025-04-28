4ab78b56a51d7cbd4bfe4ad6ef7cdf501cb439c6
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-04-28 16:37:44 +0100

    Remove mock todo data

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -8,13 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Mock data
-var todos = []types.Todo{
-	{ID: "1", Task: "Learn Go"},
-	{ID: "2", Task: "Build an API"},
-	{ID: "3", Task: "Dockerize the API"},
-}
-
 func GetTodos(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }

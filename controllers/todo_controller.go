805ba53fe086202420adf0f500f973ad24105e7f
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-04-28 16:38:06 +0100

    Update todo controller to use db repository...
    to fetch the todo data from the database

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -3,11 +3,18 @@ package controllers
 import (
 	"net/http"
 
-	"golang-todos/types"
+	"golang-todos/repositories"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetTodos(c *gin.Context) {
+	todos, err := repositories.TodoIndex()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
 	c.JSON(http.StatusOK, todos)
 }

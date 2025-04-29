be97fddb6f38e833796f1edd0d3c3a562583185b
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-04-29 20:51:28 +0100

    Add update todo request

--- types/requests/todo_requests.go
+++ types/requests/todo_requests.go
@@ -3,3 +3,10 @@ package requests
 type CreateTodoRequest struct {
 	Task string `form:"task" json:"task" validate:"required,min=1,max=255"`
 }
+
+// Remove strict requirements here so that we can update the task and is_complete fields
+// without needing to provide all fields at the same time
+type UpdateTodoRequest struct {
+	Task       string `form:"task" json:"task" validate:"max=255"`
+	IsComplete bool   `form:"is_complete" json:"is_complete" validate:"boolean"`
+}
